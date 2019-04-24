1c094d00f489e2fc90494ce0a42faead34987d96
Dicko Finus <dickofn@gmail.com>
2019-04-24 22:02:26 +0700

    styling init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,595 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <header class="header">
+      <div class="header__logo">
+        <img src="@/assets/faedah-09@2x.png" alt="faedah" class="header__logo--faedah">
+        <img
+          src="http://3.bp.blogspot.com/-ELuUyKi45cs/V-IBdJpaZ8I/AAAAAAAAFuE/1-f8_tmbcKgEFMdm4BdRfoHvsoVdztBlgCK4B/s1600/OJK%2B%2528Otoritas%2BJasa%2BKeuangan%2529%2Bvector%2Blogo.png"
+          alt="ojk"
+          class="header__logo--ojk"
+        >
+      </div>
+      <div class="header__navigation">
+        <div class="header__navigation-item header__navigation-item--dana">Dana Komunitas</div>
+        <div class="header__navigation-item header__navigation-item--pendana">Pendana</div>
+        <div class="header__navigation-item header__navigation-item--login">Log In</div>
+        <div class="header__navigation-item header__navigation-item--signup">Sign Up</div>
+        <div class="header__navigation-item header__navigation-item--lang">Bahasa</div>
+      </div>
+    </header>
+    <section class="who">
+      <div class="who__title">Kembangkan Usaha Anda Bersama Dana Komunitas</div>
+      <div
+        class="who__content"
+      >Kami akan membantu menghubungkan usaha anda dengan solusi akses pendanaan dengan transparansi dan cepat, sehingga pemberian modal menjadi lebih mudah. Bersama Dana Komunitas, semua orang memiliki kesempatan untuk melangkah menuju kesejahteraan</div>
+      <div class="who__button">Ajukan Pinjaman</div>
+    </section>
+    <section class="why">
+      <div class="why__title">Mengapa Komunitas Faedah cocok untuk anda?</div>
+      <div class="why__group">
+        <div class="why__group-item why__group-item--1">
+          <img src="@/assets/transparant@2x.png" alt="transparant">
+          <div class="why__group-item__title">Transparan</div>
+          <div class="why__group-item__text">Tidak ada biaya tersembunyi</div>
+        </div>
+        <div class="why__group-item why__group-item--2">
+          <img src="@/assets/fleksible.png" alt="transparant">
+          <div class="why__group-item__title">Fleksibel</div>
+          <div class="why__group-item__text">Pembiayaan sesuai kebutuhan</div>
+        </div>
+        <div class="why__group-item why__group-item--3">
+          <img src="@/assets/pay-online.png" alt="transparant">
+          <div class="why__group-item__title">Praktis</div>
+          <div class="why__group-item__text">Pencairan dan pembayaran berbasis online</div>
+        </div>
+        <div class="why__group-item why__group-item--4">
+          <img src="@/assets/tidak-ada-jaminan.png" alt="transparant">
+          <div class="why__group-item__title">Mudah</div>
+          <div class="why__group-item__text">Tanpa jaminan</div>
+        </div>
+      </div>
+    </section>
+    <section class="apply">
+      <div class="requirement">
+        <div class="requirement__picture">
+          <img src="@/assets/group-9@3x.png" alt="persyaratan">
+        </div>
+        <div class="requirement__text">
+          <div class="requirement__text__title">Persyaratan</div>
+          <div class="requirement__text__list">
+            <div class="requirement__text__list-item">
+              <div class="requirement__text__list-item--no">1</div>
+              <div class="requirement__text__list-item--desc">Pembiayaan Modal Kerja</div>
+            </div>
+            <div class="requirement__text__list-item">
+              <div class="requirement__text__list-item--no">2</div>
+              <div class="requirement__text__list-item--desc">Berdomisili Setempat</div>
+            </div>
+            <div class="requirement__text__list-item">
+              <div class="requirement__text__list-item--no">3</div>
+              <div
+                class="requirement__text__list-item--desc"
+              >Memiliki usaha yang sedang berjalan minimal 2 tahun</div>
+            </div>
+            <div class="requirement__text__list-item">
+              <div class="requirement__text__list-item--no">4</div>
+              <div class="requirement__text__list-item--desc">Grup ber-anggotakan min. 7 orang</div>
+            </div>
+            <div class="requirement__text__list-item">
+              <div class="requirement__text__list-item--no">5</div>
+              <div class="requirement__text__list-item--desc">Memiliki karakter yang baik</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="process">
+        <div class="process__content">
+          <div class="process__content__array">
+            <div
+              class="process__content__array-item process__content__array-item--content"
+            >Terkumpul 7-10 orang</div>
+            <div class="process__content__array-item process__content__array-item--arrow">&rarr;</div>
+            <div class="process__content__array-item process__content__array-item--content">Survey</div>
+            <div class="process__content__array-item process__content__array-item--arrow">&rarr;</div>
+            <div
+              class="process__content__array-item process__content__array-item--content"
+            >Pengarahan 3 hari</div>
+            <div class="process__content__array-item process__content__array-item--arrow">&rarr;</div>
+            <div
+              class="process__content__array-item process__content__array-item--content"
+            >Pencairan Dana</div>
+            <div class="process__content__array-item process__content__array-item--arrow">&rarr;</div>
+            <div
+              class="process__content__array-item process__content__array-item--content"
+            >Pembayaran</div>
+          </div>
+          <div class="process__content__title">Pembayaran</div>
+          <div class="process__content__list">
+            <div class="process__content__list-item">
+              <div class="process__content__list-item--no">1</div>
+              <div class="process__content__list-item--desc">Pembayaran angsuran mudah!</div>
+            </div>
+            <div class="process__content__list-item">
+              <div class="process__content__list-item--no">2</div>
+              <div
+                class="process__content__list-item--desc"
+              >Melalui PayPro, bank transfer, atau auto-debet</div>
+            </div>
+            <div class="process__content__list-item">
+              <div class="process__content__list-item--no">3</div>
+              <div class="process__content__list-item--desc">2 mingguan untuk modal 1-6 juta</div>
+            </div>
+            <div class="process__content__list-item">
+              <div class="process__content__list-item--no">4</div>
+              <div class="process__content__list-item--desc">Bulanan untuk modal 7-10 juta</div>
+            </div>
+          </div>
+        </div>
+        <div class="process__picture">
+          <img src="@/assets/376284-pbr-7-jp-367@3x.png" alt="workdesk">
+        </div>
+      </div>
+    </section>
+    <section class="faq">
+      <div class="faq__title">FAQs</div>
+      <div class="faq__content">
+        <div class="faq__content-question">
+          <div @click="a1=!a1" class="faq__content-question__q">
+            <div class="faq__content-question__q-left">
+              <div class="faq__content-question__q-left--no">1</div>
+              <div
+                class="faq__content-question__q-left--text"
+              >Berapa pembiayaan yang bisa di ajukan?</div>
+            </div>
+            <div class="faq__content-question__q-right">
+              <div class="faq__content-question__q-right--arr">{{ a1?'&uarr;':'&darr;' }}</div>
+            </div>
+          </div>
+          <transition name="slide-fade">
+            <div
+              v-if="a1"
+              class="faq__content-question__a"
+            >Pembiayaan yang bisa diajukan mulai dari 1 sampai 10 juta per orang.</div>
+          </transition>
+        </div>
+        <div class="faq__content-question">
+          <div @click="a2=!a2" class="faq__content-question__q">
+            <div class="faq__content-question__q-left">
+              <div class="faq__content-question__q-left--no">2</div>
+              <div class="faq__content-question__q-left--text">Berapa bagi hasil yang ditawarkan</div>
+            </div>
+            <div class="faq__content-question__q-right">
+              <div class="faq__content-question__q-right--arr">{{ a2?'&uarr;':'&darr;' }}</div>
+            </div>
+          </div>
+          <transition name="slide-fade">
+            <div
+              v-if="a2"
+              class="faq__content-question__a"
+            >Komunitas Faedah akan memberikan bagi hasil yang rendah sesuai dengan besarnya pinjaman modal yang didapat</div>
+          </transition>
+        </div>
+      </div>
+    </section>
+    <footer class="footer">design by Digiasia created by Dicko Finus</footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+/**
+ * THIS PAGE IS CREATED BY DICKO FINUS
+ * PLEASE CONSIDER CONTACTING ME BEFORE YOU USE IT FOR YOUR OWN PURPOSE
+ * dickofn@gmail.com
+ * 
+ * PLEASE GIVE CREDIT OR LEAVE THE FOOTER AT IT IS
+ */
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      a1: false,
+      a2: false
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.header {
+  padding: 10px 50px;
+  background-color: #fff;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media only screen and (max-width: 1000px) {
+    flex-direction: column;
+  }
+
+  &__logo {
+    display: flex;
+    align-items: center;
+
+    :not(:last-child) {
+      margin-right: 20px;
+    }
+
+    &--faedah {
+      height: 80px;
+      @media only screen and (max-width: 1000px) {
+        height: auto;
+        width: 50%;
+      }
+    }
+    &--ojk {
+      height: 80px;
+      @media only screen and (max-width: 1000px) {
+        height: auto;
+        width: 50%;
+      }
+    }
+
+    @media only screen and (max-width: 1000px) {
+      margin-bottom: 10px;
+    }
+
+    @media only screen and (max-width: 600px) {
+      margin-bottom: 0px;
+    }
+  }
+
+  &__navigation {
+    display: flex;
+
+    @media only screen and (max-width: 600px) {
+      flex-direction: column;
+      text-align: center;
+    }
+
+    :not(:last-child) {
+      margin-right: 20px;
+
+      @media only screen and (max-width: 600px) {
+        margin-right: 0;
+      }
+    }
+
+    &-item {
+      font-weight: 800;
+
+      &:hover {
+        cursor: pointer;
+        color: purple;
+      }
+    }
+  }
+}
+
+.who {
+  padding: 180px 150px;
+  @media only screen and (max-width: 1000px) {
+    padding: 50px;
+  }
+  background-image: url("~@/assets/group-7@3x.jpg");
+  background-size: cover;
+
+  @media only screen and (max-width: 1000px) {
+    text-align: center;
+  }
+
+  &__title {
+    font-size: 24px;
+    font-weight: 600;
+    color: #fff;
+    margin-bottom: 50px;
+  }
+  &__content {
+    width: 40%;
+    color: white;
+    line-height: 25px;
+    margin-bottom: 10px;
+
+    @media only screen and (max-width: 1000px) {
+      width: 100%;
+    }
+  }
+  &__button {
+    padding: 20px 40px;
+    text-align: center;
+    width: 200px;
+    border-radius: 50px;
+    background-color: darkorange;
+    color: #fff;
+    @media only screen and (max-width: 1000px) {
+      margin: auto;
+    }
+  }
+}
+
+.why {
+  padding: 180px 150px;
+  @media only screen and (max-width: 1000px) {
+    padding: 50px;
+  }
+  background-color: #f7f4fd;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  &__title {
+    font-size: 24px;
+    font-weight: 600;
+    margin-bottom: 50px;
+  }
+
+  &__group {
+    width: 100%;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+
+    @media only screen and (max-width: 1000px) {
+      flex-direction: column;
+      align-items: center;
+
+      & > :not(:last-child) {
+        margin-bottom: 30px;
+      }
+    }
+
+    &-item {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      &__title {
+        margin: 10px 0;
+        text-align: center;
+        font-weight: 600;
+      }
+      &__text {
+        text-align: center;
+      }
+    }
+  }
+}
+.apply {
+  padding: 180px 150px;
+  @media only screen and (max-width: 1000px) {
+    padding: 50px;
+  }
+
+  .requirement {
+    display: flex;
+    margin-bottom: 100px;
+
+    @media only screen and (max-width: 1000px) {
+      justify-content: center;
+    }
+
+    &__picture {
+      width: 50%;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      @media only screen and (max-width: 1000px) {
+        display: none;
+      }
+
+      img {
+        width: 50%;
+      }
+    }
+
+    &__text {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: center;
+
+      @media only screen and (max-width: 1000px) {
+        align-items: center;
+      }
+
+      &__title {
+        margin-bottom: 20px;
+        font-size: 24px;
+        font-weight: 600;
+      }
+
+      &__list {
+        display: flex;
+        flex-direction: column;
+
+        & > :not(:last-child) {
+          margin-bottom: 20px;
+        }
+
+        &-item {
+          display: flex;
+          align-items: center;
+
+          &--no {
+            margin-right: 10px;
+            padding: 5px 10px;
+            background-color: darkorange;
+            color: #fff;
+            border-radius: 100%;
+          }
+        }
+      }
+    }
+  }
+  .process {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    &__content {
+      display: flex;
+      flex-direction: column;
+
+      @media only screen and (max-width: 1000px) {
+        align-items: center;
+      }
+
+      &__array {
+        margin-bottom: 60px;
+
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+
+        @media only screen and (max-width: 1000px) {
+          justify-content: center;
+        }
+
+        &:hover {
+          cursor: default;
+        }
+
+        & > :not(:last-child) {
+          margin-right: 20px;
+        }
+
+        &-item {
+          margin-bottom: 10px;
+
+          &--content {
+            padding: 10px 30px;
+            border: 1px solid #000;
+            border-radius: 50px;
+
+            &:hover {
+              cursor: pointer;
+              background-color: lightgreen;
+            }
+          }
+        }
+      }
+
+      &__title {
+        margin-bottom: 20px;
+        font-size: 24px;
+        font-weight: 600;
+      }
+
+      &__list {
+        display: flex;
+        flex-direction: column;
+
+        & > :not(:last-child) {
+          margin-bottom: 20px;
+        }
+
+        &-item {
+          display: flex;
+          align-items: center;
+
+          &--no {
+            margin-right: 10px;
+            padding: 5px 10px;
+            background-color: darkorange;
+            color: #fff;
+            border-radius: 100%;
+          }
+        }
+      }
+    }
+
+    &__picture {
+      width: 60%;
+
+      @media only screen and (max-width: 1000px) {
+        display: none;
+      }
+      img {
+        width: 100%;
+      }
+    }
+  }
+}
+
+.faq {
+  padding: 120px 150px;
+  background-color: #f7f4fd;
+
+  @media only screen and (max-width: 1000px) {
+    padding: 50px 0;
+  }
+
+  &__title {
+    font-size: 24px;
+    font-weight: 600;
+    margin-bottom: 20px;
+
+    @media only screen and (max-width: 1000px) {
+      margin-left: 50px;
+    }
+  }
+
+  &__content {
+    width: 100%;
+
+    &-question {
+      &__q {
+        cursor: pointer;
+        padding: 20px 10px;
+        background-color: #fff;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        @media only screen and (max-width: 1000px) {
+          padding: 20px 50px;
+        }
+
+        &-left {
+          display: flex;
+          align-items: center;
+
+          &--no {
+            margin-right: 10px;
+            padding: 5px 10px;
+            background-color: darkorange;
+            color: #fff;
+            border-radius: 100%;
+          }
+        }
+      }
+      &__a {
+        padding: 20px 10px;
+
+        @media only screen and (max-width: 1000px) {
+          padding: 20px 50px;
+        }
+      }
+    }
+  }
+}
+
+.footer {
+  text-align: center;
+  background-color: purple;
+  color: #f7f4fd;
+}
+
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
